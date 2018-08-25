fe5da014453a81d41efe4a8a4104407904777032
Joel_Dynamo <dynamo.joey@gmail.com>
2018-08-25 02:48:37 -0700

    Initial commit

---
+++ app/src/main/java/com/example/joel/navdrawers/MessageFragment.java
@@ -0,0 +1,23 @@
+package com.example.joel.navdrawers;
+
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MessageFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_mesage, container, false);
+
+
+        return view;
+    }
+}
